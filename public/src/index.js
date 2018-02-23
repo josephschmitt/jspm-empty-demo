862d70ecd6173ef855c793b6f7e6f19de1f30c73
Joe Schmitt <joe@compass.com>
2018-02-23 13:44:13 -0500

    Initial commit

---
+++ public/src/index.js
@@ -0,0 +1,5 @@
+import dependency from '@joe-sh/jspm-empty-dependency';
+import otherDependency from '@joe-sh/jspm-empty-other-dependency/index.js';
+
+dependency();
+otherDependency();
---
+++ public/src/other-dependency/index.js
@@ -0,0 +1,3 @@
+export default function() {
+  console.log('jspm-empty-other-dependency');
+}
